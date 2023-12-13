b6487f301dbe75c1c818c409b0f3b1e5f46811d4
Vlada <vladimir.davidovic@thoughtworks.com>
2023-12-13 11:14:58 +0000

    ch03 Lifecycle Methods
    Different approaches to testong lifecycle.
    per_method (e.g. @BeforeEach) vs. per_class (e.g. @BeforeAll)

---
+++ src/main/java/com/vld/wiredbraincoffee/reward/RewardInformation.java
@@ -0,0 +1,30 @@
+package com.vld.wiredbraincoffee.reward;
+
+public class RewardInformation {
+
+    private long pointsRedeemed;
+    private double discount;
+
+    public RewardInformation() { }
+
+    public RewardInformation(long pointsRedeemed, double discount) {
+        this.pointsRedeemed = pointsRedeemed;
+        this.discount = discount;
+    }
+
+    public long getPointsRedeemed() {
+        return pointsRedeemed;
+    }
+
+    public void setPointsRedeemed(long pointsReedemed) {
+        this.pointsRedeemed = pointsReedemed;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+}
---
+++ src/main/java/com/vld/wiredbraincoffee/reward/RewardService.java
@@ -0,0 +1,32 @@
+package com.vld.wiredbraincoffee.reward;
+
+import com.vld.wiredbraincoffee.product.Product;
+
+import java.util.List;
+
+public abstract class RewardService {
+
+    protected long neededPoints;
+
+    public abstract RewardInformation applyReward(
+        List<Product> order, long customerPoints
+    );
+
+    protected double calculateTotal(List<Product> order) {
+        double sum = 0;
+
+        if (order != null) {
+            sum = order.stream().mapToDouble(Product::getPrice).sum();
+        }
+
+        return sum;
+    }
+
+    public long getNeededPoints() {
+        return neededPoints;
+    }
+
+    public void setNeededPoints(long neededPoints) {
+        this.neededPoints = neededPoints;
+    }
+}
